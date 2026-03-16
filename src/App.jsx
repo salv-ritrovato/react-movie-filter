8cf413f3c24a577ea2c016691835d2bb09691482
salv-ritrovato <salv.ritrovato@gmail.com>
2026-03-16 12:44:40 +0100

    task completed, working on style

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,36 @@ function App() {
     { title: 'Pulp Fiction', genre: 'Thriller' }
   ];
 
-  const [movie, setMovie] = useState("");
+  const [genre, setGenre] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+
+  useEffect(() => {
+    if (genre === "") {
+      setFilteredMovies(movies);
+    } else {
+      setFilteredMovies(movies.filter(movie => movie.genre === genre))
+    }
+  }, [genre, movies]);
 
   return (
     <>
       <div className="container">
-        <h1>MyMovieList <i className="bi bi-film"></i></h1>
+        <h1 className='mt-3'>MyMovieList <i className="bi bi-film"></i></h1>
+
+        <select name="movies" value={genre} onChange={(e) => setGenre(e.target.value)}>
+          <option value="">Seleziona un genere...</option>
+          <option value="Fantascienza">Fantascienza</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Romantico">Romantico</option>
+          <option value="Azione">Azione</option>
+        </select>
+
         <div>
-          <ol className='list-unstyled'>
-            {movies.map(movie => (
-              <li>TITOLO: {movie.title} GENERE: {movie.genre}</li>
+          {/* Ordered list with dynamically printed movies */}
+          <ol className='list-unstyled mt-3'>
+            {/* Printing my array with map method */}
+            {filteredMovies.map(movie => (
+              <li key={movie.title}>TITOLO: {movie.title} | GENERE: {movie.genre}</li>
             ))}
           </ol>
         </div>
